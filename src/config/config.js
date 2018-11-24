92b760faf71f22e8f979ade39cd352dd8b630408
cander <cander_liubiao@126.com>
2018-11-25 00:20:34 +0800

    config

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-// const baseUrl = 'http://192.168.1.55:10001'
-const baseUrl = 'http://127.0.0.1:10001'
-
-const g = {
-  uploadUrl: baseUrl + '/uploadimg',
-  apiUrl: baseUrl
-}
-
-export default g
